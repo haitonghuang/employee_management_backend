4f60ecda2ff9d151776745253acba84fe5f8333c
Jessiehuang <haitong0914@gmail.com>
2024-06-17 14:23:00 +0800

    fix cors error

--- src/main/java/com/employee/employee_management/EmployeeController.java
+++ src/main/java/com/employee/employee_management/EmployeeController.java
@@ -2,19 +2,22 @@ package com.employee.employee_management;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 
-
 @RestController
+@CrossOrigin(origins = "http://localhost:5173",  allowCredentials="true", methods={RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class EmployeeController {
     @Autowired
     EmployeeService employeeServiceImpl;
@@ -32,6 +35,17 @@ public class EmployeeController {
         // }
         // return ResponseEntity.ok(employee.get());
         return employeeServiceImpl.findEmployeeById(id);
+
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployee(){
+        // Optional<Employee> employee = Optional.of(employeeServiceImpl.findEmployeeById(id));
+        // if(!employee.isPresent()){
+        //     throw new ResourceNotFoundException("Employee Not Found");
+        // }
+        // return ResponseEntity.ok(employee.get());
+        return employeeServiceImpl.findAllEmployees();
 
     }
 
