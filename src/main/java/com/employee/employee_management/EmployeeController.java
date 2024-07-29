bf50f29cf603073f76c05d2ab380317db2377392
jessie wong <jessie.wong@alphasolution.com.mo>
2024-07-29 23:12:26 +0800

    create controller for deleting employee

--- src/main/java/com/employee/employee_management/EmployeeController.java
+++ src/main/java/com/employee/employee_management/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,12 @@ public class EmployeeController {
         return employeeServiceImpl.findAllEmployees();
 
     }
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id){
+        employeeServiceImpl.removeEmployeeById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 
 
 }
